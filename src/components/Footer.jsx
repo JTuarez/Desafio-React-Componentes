9695176c686ff0d990f842e0384a49c48a9b043c
JTuarez <jordyalexjatr@hotmail.comgit config --global user.name JTuarezgit config --global user.email jordyalexjatr@hotmail.comgit config --global user.email jordyalexjatr@hotmail.com>
2025-12-16 17:13:14 -0300

    cambio-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-auto">
       <small>
-        © 2021 - Pizzería Mamma Mia! - Todos los derechos reservados
+        © 2025 - Pizzería Mamma Mia! - Todos los derechos reservados
       </small>
     </footer>
   );
